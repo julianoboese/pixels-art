0a53bf859b54def352e918fe4cc96eb14dd63677
Juliano Boese <juliano.boese@gmail.com>
2021-11-22 22:13:22 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 const board = document.getElementById('pixel-board');
-window.onload = createBoard(5);
 
-
-
-function selectColor() {
-  const palette = document.getElementById('color-palette');
-  palette.addEventListener('click', (event) => {
-    const selected = document.querySelector('.selected');
-    selected.classList.remove('selected');
-    event.target.classList.add('selected');
-  });
+function generateRandomColor() {
+  document.getElementById('black').style.backgroundColor = 'black';
+  const randomColor1 = Math.floor(Math.random() * 16777215).toString(16);
+  const randomColor2 = Math.floor(Math.random() * 16777215).toString(16);
+  const randomColor3 = Math.floor(Math.random() * 16777215).toString(16);
+  document.getElementById('color1').style.backgroundColor = `#${randomColor1}`;
+  document.getElementById('color2').style.backgroundColor = `#${randomColor2}`;
+  document.getElementById('color3').style.backgroundColor = `#${randomColor3}`;
 }
-Math.random()
 
-selectColor();
+function checkInput(inputValue) {
+  const side = inputValue;
+  if (side < 5) {
+    return 5;
+  } if (side > 50) {
+    return 50;
+  }
+  return side;
+}
 
-function paintPixel() {
-  board.addEventListener('click', (event) => {
-    const selectedElement = document.querySelector('.selected');
-    const selectedColor = selectedElement.id;
-    if (event.target.className === 'pixel') {
-      event.target.style.backgroundColor = selectedColor;
+function createBoard(N) {
+  const side = checkInput(N);
+  for (let i = 1; i <= side; i += 1) {
+    const pixelLine = document.createElement('div');
+    for (let j = 1; j <= side; j += 1) {
+      const pixelSquare = document.createElement('div');
+      pixelSquare.className = 'pixel';
+      pixelSquare.style.backgroundColor = 'white';
+      pixelLine.appendChild(pixelSquare);
     }
-  });
+    board.appendChild(pixelLine);
+  }
 }
 
-paintPixel();
-
-function clearBoard() {
-  const clearButton = document.getElementById('clear-board');
-  clearButton.addEventListener('click', () => {
-    const allPixels = document.getElementsByClassName('pixel');
-    for (let i = 0; i < allPixels.length; i += 1) {
-      allPixels[i].style.backgroundColor = 'white';
-    }
-  });
-}
-
-clearBoard();
+window.addEventListener('load', () => {
+  createBoard(5);
+  generateRandomColor();
+});
 
 function changeBoardSize() {
   const generateButton = document.getElementById('generate-board');
@@ -58,26 +58,38 @@ function changeBoardSize() {
 
 changeBoardSize();
 
-function createBoard(N) {
-  const side = checkInput(N);
-  for (let i = 1; i <= side; i += 1) {
-    const pixelLine = document.createElement('div');
-    for (let j = 1; j <= side; j += 1) {
-      const pixelSquare = document.createElement('div');
-      pixelSquare.className = 'pixel';
-      pixelSquare.style.backgroundColor = 'white';
-      pixelLine.appendChild(pixelSquare);
+function selectColor() {
+  const palette = document.getElementById('color-palette');
+  palette.addEventListener('click', (event) => {
+    const selected = document.querySelector('.selected');
+    selected.classList.remove('selected');
+    event.target.classList.add('selected');
+  });
+}
+Math.random();
+
+selectColor();
+
+function paintPixel() {
+  board.addEventListener('click', (event) => {
+    const selectedElement = document.querySelector('.selected');
+    const selectedColor = selectedElement.style.backgroundColor;
+    if (event.target.className === 'pixel') {
+      event.target.style.backgroundColor = selectedColor;
     }
-    board.appendChild(pixelLine);
-  }
+  });
 }
 
-function checkInput(inputValue) {
-  const side = inputValue;
-  if (side < 5) {
-    return 5;
-  } if (side > 50) {
-    return 50;
-  }
-  return side;
+paintPixel();
+
+function clearBoard() {
+  const clearButton = document.getElementById('clear-board');
+  clearButton.addEventListener('click', () => {
+    const allPixels = document.getElementsByClassName('pixel');
+    for (let i = 0; i < allPixels.length; i += 1) {
+      allPixels[i].style.backgroundColor = 'white';
+    }
+  });
 }
+
+clearBoard();
